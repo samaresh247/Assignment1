80cb2441c059d2dd092d58014e735d3741d1af18
samaresh247 <samareshmondal832@gmail.com>
2023-05-18 11:41:52 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,83 @@
+// ১. Variable কি ?
+
+//  var variAble="Variables are containers for storing data values. In Java, there are different types of variables, for example: String - stores text, such as "Hello". String values are surrounded by double quotes."
+
+// ২. Variable কিভাবে লিখতে হয় ?
+
+//var myFd="Hablu Vaiya";
+//console.log(myFd)
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+// In Java, a string is a sequence of characters.We use double quotes to represent a string in Java. For Ex: 
+// var myName="Samaresh";
+// looks output:
+// console.log(myName)
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+// Java Number class is an abstract class which is placed in java.This class contains a single consructor number.
+// var num1=45;
+// var num2=34;
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+// In computer science, the Boolean is a data type that has one of two possible values (usually denoted true and false) which is intended to represent the two truth values of logic and Boolean algebra.
+ 
+// var BoolType=ture;
+// console.log(BoolType)
+// var BoolType=false;
+// console.log(BoolType)
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+// var letter="The Daily Star is the most trusted Bangladeshi English-language daily newspaper. It is by far the largest circulating English-language newspaper in the country";
+// console.log(letter.toUpperCase())
+
+// var letter="The Daily Star is the most trusted Bangladeshi English-language daily newspaper. It is by far the largest circulating English-language newspaper in the country";
+// console.log(letter.toLowerCase())
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+// There are 8 operators in Java
+// Unary Operator,
+// Arithmetic Operator,
+// Shift Operator,
+// Relational Operator,
+// Bitwise Operator,
+// Logical Operator,
+// Ternary Operator and.
+// Assignment Operator.
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+
+//  var num1=-20;
+// var sum=math.abs(num1);
+// console.log(sum)
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন ।
+
+//  var num1=.4647
+//  var sum=Math.ceil(num1);
+//  console.log(sum)
+//  Showing output
+//     1
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+// var num=0.98789;
+// var mot=Math.floor(num);
+// console.log(mot)
+// Showing output
+//     0
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+//  var totalMembers=10;
+//  var voter=5.545;
+//  var sum=Math.round(voter)
+//  console.log(sum)
+//Showing output
+    6
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+//  var lotery=Math.random();
+//  console.log(lotery)
+//  output 
+//  0.6567674567
+// asignment.js - GitHub Link 
